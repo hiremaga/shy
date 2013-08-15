a7411b93fa796396180f5e77249e10ea9f98f80a
Abhi Hiremagalur <abhi@pivotallabs.com>
2013-08-14 21:02:11 -0700

    Discard Struct inspired syntax

--- lib/shy.rb
+++ lib/shy.rb
@@ -5,14 +5,6 @@ module Shy
     klass.extend(DSL)
   end
 
-  def self.new(*attribute_names)
-    warn 'Shy.new is deprecated, please use the Shy::DSL style instead.'
-    Class.new do
-      include Shy
-      shy *attribute_names
-    end
-  end
-
   def initialize(attributes={})
     attributes.each do |name, value|
       send("#{name}=", value)
--- spec/shy_spec.rb
+++ spec/shy_spec.rb
@@ -1,54 +1,34 @@
 require 'spec_helper'
 
 describe Shy do
-  shared_examples_for 'a Shy class' do
-    it { should respond_to(:shy) }
+  subject do
+    Class.new do
+      include Shy
 
-    it "can be instantiated with a hash of its attributes' values" do
-      shy_instance = subject.new(foo: 'Foo')
-      shy_instance.send(:foo).should eq('Foo')
-      shy_instance.send(:bar).should be_nil
-
-      expect {
-        shy_instance.send(:bar=, 'Bar')
-      }.to change { shy_instance.send(:bar) }.to('Bar')
-    end
-
-    its(:private_instance_methods) { should include(:foo, :foo=, :bar, :bar=) }
-
-    it "allows its accessors to be optionally publicized" do
-      expect {
-        subject.class_eval do
-          public :foo
-          public :bar=
-        end
-      }.to change { subject.public_instance_methods(false) }.to([:foo, :bar=])
+      shy :foo, :bar
     end
   end
 
-  context 'when using the deprecated Struct inspired syntax' do
-    subject do
-      Shy.new(:foo, :bar)
-    end
+  it { should respond_to(:shy) }
 
-    before do
-      Shy.should_receive(:warn)
-    end
+  it "can be instantiated with a hash of its attributes' values" do
+    shy_instance = subject.new(foo: 'Foo')
+    shy_instance.send(:foo).should eq('Foo')
+    shy_instance.send(:bar).should be_nil
 
-    it { should be_a(Class) }
-
-    it_behaves_like 'a Shy class'
+    expect {
+      shy_instance.send(:bar=, 'Bar')
+    }.to change { shy_instance.send(:bar) }.to('Bar')
   end
 
-  context 'when using the mixin' do
-    subject do
-      Class.new do
-        include Shy
+  its(:private_instance_methods) { should include(:foo, :foo=, :bar, :bar=) }
 
-        shy :foo, :bar
+  it "allows its accessors to be optionally publicized" do
+    expect {
+      subject.class_eval do
+        public :foo
+        public :bar=
       end
-    end
-
-    it_behaves_like 'a Shy class'
+    }.to change { subject.public_instance_methods(false) }.to([:foo, :bar=])
   end
 end
